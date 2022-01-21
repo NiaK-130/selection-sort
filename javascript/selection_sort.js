211a5dc31f8be2c9e213cfd6ed1ddb5d71af68df
Nia K <nsjkappen@gmail.com>
2022-01-21 12:55:17 -0800

    commit

--- javascript/selection_sort.js
+++ javascript/selection_sort.js
@@ -1,5 +1,18 @@
 function selectionSort(arr) {
-  // type your code here
+  for (let i = 0; i < arr.length; i++){
+    let min = i;
+
+    for (let j = i+1; j < arr.length; j++) {
+      if(arr[j] < arr[min]){
+        min = j 
+      }
+    }
+      if (i !== min){
+        [arr[i], arr[min]] = [arr[min], arr[i]]
+      
+    }
+  }
+  return arr;
 }
 
 if (require.main === module) {
